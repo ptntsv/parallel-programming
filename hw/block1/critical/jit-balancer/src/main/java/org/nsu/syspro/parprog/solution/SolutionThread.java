752d6673988093d96483c92480b282231ccf8e4d
ptntsv <hitogavax@ya.ru>
2025-04-03 11:14:40 +0700

    Easy constraints

--- hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
+++ hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
@@ -4,25 +4,37 @@ import org.nsu.syspro.parprog.UserThread;
 import org.nsu.syspro.parprog.external.*;
 
 import java.util.*;
-import java.util.concurrent.*;
 
 public class SolutionThread extends UserThread {
 
-    // TODO: add fields here!
+    private final Map<Long, Long> localHotness = new HashMap<>();
+    static final Map<Long, CompiledMethod> globalL1 = new HashMap<>();
 
     public SolutionThread(int compilationThreadBound, ExecutionEngine exec, CompilationEngine compiler, Runnable r) {
         super(compilationThreadBound, exec, compiler, r);
-        // TODO: initialize fields!
     }
 
+    private void updateL1(MethodID methodID) {
+        CompiledMethod code = compiler.compile_l1(methodID);
+        synchronized (globalL1) {
+            globalL1.put(methodID.id(), code);
+        }
+    }
 
     @Override
-    public ExecutionResult executeMethod(MethodID id) {
-        // TODO: implement me
-        return null;
-    }
+    public ExecutionResult executeMethod(MethodID methodID) {
+        final Long id = methodID.id();
+        long lHotLevel = localHotness.getOrDefault(id, 0L);
+        localHotness.put(id, lHotLevel + 1);
 
-    // TODO: add methods
-    // TODO: add inner classes
-    // TODO: add utility classes in the same package
+        CompiledMethod code;
+        synchronized (globalL1) {
+            code = globalL1.getOrDefault(methodID.id(), null);
+        }
+        if (code != null) return exec.execute(code);
+        if (lHotLevel > 9_000) {
+            updateL1(methodID);
+        }
+        return exec.interpret(methodID);
+    }
 }
