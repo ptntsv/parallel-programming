b4c76035a8c0fbea83d544eb7118b8d287aeed8f
ptntsv <hitogavax@ya.ru>
2025-04-03 20:37:54 +0700

    Kinda 'documentation'

--- hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
+++ hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
@@ -4,15 +4,16 @@ import org.nsu.syspro.parprog.UserThread;
 import org.nsu.syspro.parprog.external.*;
 
 import java.util.*;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SolutionThread extends UserThread {
 
+    /**
+     * Thread factory utility class.
+     */
     static class SolutionThreadFactory implements ThreadFactory {
         @Override
         public Thread newThread(Runnable runnable) {
@@ -21,13 +22,38 @@ public class SolutionThread extends UserThread {
     }
 
     private final ThreadFactory threadFactory;
+    /**
+     * Thread local field that contains frequency of execution of certain method.
+     */
     private final Map<Long, Long> localHotness = new HashMap<>();
+    /**
+     * Thread global caching map that contains precompiled methods with L1 compiler.
+     */
     static final Map<Long, CompiledMethod> globalL1 = new HashMap<>();
+    /**
+     * Thread global caching map that contains precompiled methods with L2 compiler.
+     */
     static final Map<Long, CompiledMethod> globalL2 = new HashMap<>();
+    /**
+     * Buffer used by specific thread for L2 compilation.
+     */
     static final Deque<CompiledMethod> compilingDone = new ArrayDeque<>();
+    /**
+     * Lock for compilingDone.
+     */
     static Lock lock = new ReentrantLock();
+    /**
+     * Conditional variable for compilingDone.
+     */
     static Condition condition = lock.newCondition();
 
+
+    /**
+     * Method that compiles(L2) given method, produces it into buffer and notifies.
+     *
+     * @param methodID Method to compile.
+     * @param N        Maximum size of compilingDone buffer.
+     */
     private void produceL2Compilation(MethodID methodID, long N) {
         lock.lock();
         try {
@@ -50,6 +76,11 @@ public class SolutionThread extends UserThread {
         threadFactory = new SolutionThreadFactory();
     }
 
+    /**
+     * Method that updates global cache with L1 compiled methods.
+     *
+     * @param methodID Method to compile.
+     */
     private void updateL1(MethodID methodID) {
         CompiledMethod code = compiler.compile_l1(methodID);
         synchronized (globalL1) {
@@ -57,6 +88,11 @@ public class SolutionThread extends UserThread {
         }
     }
 
+    /**
+     * Consumes L2 compiled methods from buffer and updates global cache.
+     *
+     * @param methodID Method to compile.
+     */
     private void updateL2(MethodID methodID) {
         lock.lock();
         try {
@@ -103,6 +139,9 @@ public class SolutionThread extends UserThread {
         return exec.interpret(methodID);
     }
 
+    /**
+     * Specific thread to L2 compilation.
+     */
     static class L2CompilerThread extends Thread {
         private final Runnable runnable;
 
